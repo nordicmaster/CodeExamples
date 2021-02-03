d27a52ecb692ca8e69daac7278150d2d4c709751
nordicmaster <32869220+nordicmaster@users.noreply.github.com>
2021-02-03 21:18:58 +0300

    Add files via upload

---
+++ Converts (base64).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common.Helpers
+{
+    public static class Converts
+    {
+        public static string ToBase64(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static string FromBase64(string text)
+        {
+            byte[] bytes = Convert.FromBase64String(text);
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
---
+++ LinqExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DataBase.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<TResult> If<TResult>(this IEnumerable<TResult> source, bool cond, Func<TResult, bool> func)
+        {
+            if (cond)
+            {
+                source = source.Where(func);
+            }
+
+            return source;
+        }
+
+        public static IOrderedEnumerable<TResult> OrderBy<TResult>(this IQueryable<TResult> source, Func<TResult, object> func, bool desc)
+        {
+            return desc ? source.AsEnumerable().OrderByDescending(func) : source.AsEnumerable().OrderBy(func);
+        }
+    }
+}
